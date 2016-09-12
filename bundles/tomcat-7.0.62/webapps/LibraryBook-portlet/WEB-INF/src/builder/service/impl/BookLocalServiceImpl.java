25a44e15ae3e50ce0344339a58c49aa8c169b2c1
Parkash Kumar <parkash.kumar@globalnorthstar.com>
2016-09-12 18:38:15 +0500

    Finder methods implementation in BookLocalServiceImpl.java

--- bundles/tomcat-7.0.62/webapps/LibraryBook-portlet/WEB-INF/src/builder/service/impl/BookLocalServiceImpl.java
+++ bundles/tomcat-7.0.62/webapps/LibraryBook-portlet/WEB-INF/src/builder/service/impl/BookLocalServiceImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -16,33 +16,47 @@ package builder.service.impl;
 
 import java.util.List;
 
-import com.liferay.portal.kernel.exception.SystemException;
-
 import builder.model.Book;
 import builder.service.base.BookLocalServiceBaseImpl;
+import builder.service.persistence.BookUtil;
 
 /**
- * The implementation of the book local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link builder.service.BookLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
+ * The implementation of the book local service. <p> All custom service methods should be put in this class. Whenever
+ * methods are added, rerun ServiceBuilder to copy their definitions into the {@link builder.service.BookLocalService}
+ * interface. <p> This is a local service. Methods of this service will not have security checks based on the propagated
+ * JAAS credentials because this service can only be accessed from within the same VM. </p>
+ * 
  * @author Sasha
  * @see builder.service.base.BookLocalServiceBaseImpl
  * @see builder.service.BookLocalServiceUtil
  */
 public class BookLocalServiceImpl extends BookLocalServiceBaseImpl {
-	
-	public List<Book> getByAuthor(long authorId) throws SystemException {
-	    return getBookPersistence().findByAuthor(authorId);
+
+	/*
+	 * NOTE FOR DEVELOPERS: Never reference this interface directly. Always use {@link
+	 * builder.service.BookLocalServiceUtil} to access the book local service.
+	 */
+	public List<Book> getByAuthor(long authorId) {
+
+		try {
+			return BookUtil.findByAuthor(authorId);
+		}
+		catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+		return null;
 	}
-	
-	public long countByAuthor(long authorId) throws SystemException {
-	    return getBookPersistence().countByAuthor(authorId);
+
+	public int countByAuthor(long authorId) {
+
+		try {
+			return BookUtil.countByAuthor(authorId);
+		}
+		catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+		return 0;
 	}
-	
 }
